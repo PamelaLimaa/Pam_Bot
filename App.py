59342e3a22381423c5effeedcb5a5c9d49323ea7
Pamela Lima <layanepamela98@gmail.com>
2023-07-24 11:08:28 -0300

    App.py

--- App.py
+++ App.py
@@ -3,7 +3,10 @@ from slack_bolt import App
 from pathlib import Path
 from dotenv import load_dotenv
 from slack_sdk import WebClient
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from drive import copy_file_move_folder_generate_link
+from docs import inserir_texto
 
 env_path = Path('.') / '.env'
 load_dotenv(override=True)
@@ -17,29 +20,35 @@ app = App(
 # Função para escutar o canal e responder com o link do arquivo
 @app.event('message')
 def update_home_tab(client, event):
-    # Chamando a função para gerar o link do arquivo
-    file_link = copy_file_move_folder_generate_link()
+    number_incident = 928373
+    incident = 'testando'
+    descricao = 'Alguns clientes não conseguem fazer o bot rodar'
 
-    # Criando o bloco de mensagem com o link do arquivo
+    link, new_id = copy_file_move_folder_generate_link()
+
+    inserir_texto(new_id, number_incident, incident, descricao)
+
+    # bloco de mensagem com o link do arquivo
     attachments =  [
-		{
-			"color": "#f2c744",
-			"blocks": [
-				{
-					"type": "section",
-					"fields": [
-					{
-						"type": "mrkdwn",
-						"text": f" *Número*: INC\n *Incidente:* \n *Impacto*: Alguns clientes não conseguem fazer o bot rodar\n *Diagnóstico*: Instabilidade no Bot da Pam\n *Sala de Crise*: <http://meet.google.com/new|http://meet.google.com/new>\n *PostMortem*: <{file_link}|_Clique Aqui_>\n *Comunica NOC*: Clique Aqui"
-					}
-				]
-			}
-		]
-	}
-]
+        {
+            "color": "#f2c744",
+            "blocks": [
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": f" *Número*:INC{number_incident}\n *Incidente: {incident}* \n *Impacto*: {descricao}\n *Diagnóstico*: Instabilidade no Bot da Pam\n *Sala de Crise*: <http://meet.google.com/new|http://meet.google.com/new>\n *PostMortem*: <{link}|_Clique Aqui_>\n *Comunica NOC*: Clique Aqui"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 
-    # Enviando a mensagem com o link do arquivo
+    # Enviando a mensagem no canal
     client.chat_postMessage(channel=event['channel'], attachments=attachments, text='✅ *FECHADO - 14/07 a 04/07*')
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
+
